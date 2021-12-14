8483403ff275db7727e31022627c8be31bbbf2a5
bobbingbobb <lqqooynneb@gmail.com>
2021-12-14 17:20:05 +0800

    cuda_ver error

--- scripts/cuda_ver.py
+++ scripts/cuda_ver.py
@@ -6,231 +6,54 @@ from collections import namedtuple, defaultdict
 import copy as c
 import random as r
 
+from numba import jit
+
+from ik_simulator import IKTable, Robot
+
 from scipy.spatial import KDTree
 
 DATA_FOLDER = '../data/'
 RAW_DATA_FOLDER = DATA_FOLDER+'raw_data/'
 TABLE_FOLDER = DATA_FOLDER+'table/'
+# x = np.arange(100).reshape(10, 10)
+#
+# @jit(nopython=True) # Set "nopython" mode for best performance, equivalent to @njit
+# def go_fast(a): # Function is compiled to machine code when called the first time
+#     def aa(a):
+#         trace = 0.0
+#         for i in range(a.shape[0]):   # Numba likes loops
+#             trace += np.tanh(a[i, i]) # Numba likes NumPy functions
+#         return trace
+#     trace = aa(a)
+#     return a + trace              # Numba likes NumPy broadcasting
+#
+# print(go_fast(x))
 
-class Robot:
-    def __init__(self):
-        #range
-        #x: -855 ~ 855, 1710
-        #y: -855 ~ 855, 1710
-        #z: -360 ~ 1190, 1550
-        self.joint_num = 7
-        restrict = namedtuple('restrict', ['max', 'min'])
-        self.joints = [restrict(2.8973, -2.8973), restrict(1.7628, -1.7628), restrict(2.8973, -2.8973), restrict(0.0698, -3.0718), restrict(2.8973, -2.8973), restrict(3.7525, -0.0175), restrict(2.8973, -2.8973)]
-        self.dh = np.array([[0.0,     0.0, 0.333,     0.0],\
-                            [0.0,     0.0,   0.0, -m.pi/2],\
-                            [0.0,     0.0, 0.316,  m.pi/2],\
-                            [0.0,  0.0825,   0.0,  m.pi/2],\
-                            [0.0, -0.0825, 0.384, -m.pi/2],\
-                            [0.0,     0.0,   0.0,  m.pi/2],\
-                            [0.0,   0.088, 0.107,  m.pi/2]])
+@jit
+def fk_dh(joints):
+    dh = np.array([[0.0,     0.0, 0.333,     0.0],\
+                   [0.0,     0.0,   0.0, -m.pi/2],\
+                   [0.0,     0.0, 0.316,  m.pi/2],\
+                   [0.0,  0.0825,   0.0,  m.pi/2],\
+                   [0.0, -0.0825, 0.384, -m.pi/2],\
+                   [0.0,     0.0,   0.0,  m.pi/2],\
+                   [0.0,   0.088, 0.107,  m.pi/2]])
+    dh[:,0] = joints
 
-    def __rotate_z(self, angle:float):
-        rz = np.array([[m.cos(angle), -m.sin(angle), 0.0, 0.0],\
-                       [m.sin(angle), m.cos(angle), 0.0, 0.0],\
-                       [0.0, 0.0, 1.0, 0.0],\
-                       [0.0, 0.0, 0.0, 1.0]])
-        return rz
+    fk_mat = np.eye(4)
+    for i in range(7):
+        dh_mat = [[m.cos(dh[i,0])                    , -m.sin(dh[i,0])                    ,  0                  ,  dh[i,1]                    ],\
+                  [m.sin(dh[i,0])*m.cos(dh[i,3]),  m.cos(dh[i,0])*m.cos(dh[i,3]), -m.sin(dh[i,3]), -dh[i,2]*m.sin(dh[i,3])],\
+                  [m.sin(dh[i,0])*m.sin(dh[i,3]),  m.cos(dh[i,0])*m.sin(dh[i,3]),  m.cos(dh[i,3]),  dh[i,2]*m.cos(dh[i,3])],\
+                  [0                                      ,  0                                      ,  0                  ,  1                               ]]
+        fk_mat = np.dot(fk_mat, dh_mat)
+        # print(fk_mat[:3,3])
 
-    def __rotate_y(self, angle:float):
-        ry = np.array([[m.cos(angle), 0.0, m.sin(angle), 0.0],\
-                       [0.0, 1.0, 0.0, 0.0],\
-                       [-m.sin(angle), 0.0, m.cos(angle), 0.0],\
-                       [0.0, 0.0, 0.0, 1.0]])
-        return ry
+    return fk_mat[:3,3].tolist()
 
-    def __rotate_x(self, angle:float):
-        rx = np.array([[1.0, 0.0, 0.0, 0.0],\
-                       [0.0, m.cos(angle), -m.sin(angle), 0.0],\
-                       [0.0, m.sin(angle), m.cos(angle), 0.0],\
-                       [0.0, 0.0, 0.0, 1.0]])
-        return rx
-
-    def fk_dh(self, joints:list):
-        self.dh[:,0] = joints
-
-        fk_mat = np.eye(4)
-        for i in range(self.joint_num):
-            dh_mat = [[m.cos(self.dh[i,0])                    , -m.sin(self.dh[i,0])                    ,  0                  ,  self.dh[i,1]                    ],\
-            		  [m.sin(self.dh[i,0])*m.cos(self.dh[i,3]),  m.cos(self.dh[i,0])*m.cos(self.dh[i,3]), -m.sin(self.dh[i,3]), -self.dh[i,2]*m.sin(self.dh[i,3])],\
-            		  [m.sin(self.dh[i,0])*m.sin(self.dh[i,3]),  m.cos(self.dh[i,0])*m.sin(self.dh[i,3]),  m.cos(self.dh[i,3]),  self.dh[i,2]*m.cos(self.dh[i,3])],\
-            		  [0                                      ,  0                                      ,  0                  ,  1                               ]]
-            fk_mat = np.dot(fk_mat, dh_mat)
-            # print(fk_mat[:3,3])
-
-        return fk_mat[:3,3].tolist()
-
-    def fk_jo(self, joints:list):
-        #show position of every joint
-        jo = np.array([[    0.0,    0.0,   0.14,     0.0],\
-                       [    0.0,    0.0,  0.193, -m.pi/2],\
-                       [    0.0, -0.193,    0.0,  m.pi/2],\
-                       [ 0.0825,    0.0,  0.123,  m.pi/2],\
-                       [-0.0825, 0.1245,    0.0, -m.pi/2],\
-                       [    0.0,    0.0, 0.2595,  m.pi/2],\
-                       [  0.088, -0.107,    0.0,  m.pi/2]])
-
-
-        fk_mat = np.eye(4)
-        trans_mat = np.eye(4)
-        pos = []
-
-        #joints
-        for i in range(self.joint_num):
-            for j in range(3):
-                trans_mat[j,3] = jo[i,j]
-            fk_mat = np.dot(fk_mat, trans_mat)
-            fk_mat = np.dot(fk_mat, self.__rotate_x(jo[i, 3]))
-            fk_mat = np.dot(fk_mat, self.__rotate_z(joints[i]))
-            pos.append(fk_mat[:3,3].tolist())
-            # print(fk_mat[:3,3].tolist())
-
-        return pos
-
-
-class DataCollection:
-    def __init__(self):
-        self.robot = Robot()
-        self.joints = self.robot.joints
-        self.scale = 30 * m.pi/180
-        # self.filename = RAW_DATA_FOLDER+'raw_data.npz'
-
-    def without_colliding_detect(self, filename='raw_data'):
-        # self.filename = RAW_DATA_FOLDER+filename+'.npz'
-        filename = RAW_DATA_FOLDER+filename+'.npz'
-        start = d.datetime.now()
-
-        data_joints = []
-        data_positions = []
-        for j1 in range(int(self.joints[0].min*10), int(self.joints[0].max*10), int(self.scale*10)):
-            for j2 in range(int(self.joints[1].min*10), int(self.joints[1].max*10), int(self.scale*10)):
-                for j3 in range(int(self.joints[2].min*10), int(self.joints[2].max*10), int(self.scale*10)):
-                    for j4 in range(int(self.joints[3].min*10), int(self.joints[3].max*10), int(self.scale*10)):
-                        for j5 in range(int(self.joints[4].min*10), int(self.joints[4].max*10), int(self.scale*10)):
-                            for j6 in range(int(self.joints[5].min*10), int(self.joints[5].max*10), int(self.scale*10)):
-                                joints = [j1/10.0, j2/10.0, j3/10.0, j4/10.0, j5/10.0, j6/10.0, 0.0]
-                                # position = self.robot.fk_dh(joints)
-                                position = self.robot.fk_jo(joints)
-                                for i, j in enumerate(position):
-                                    for p, n in enumerate(j):
-                                        position[i][p] = round(n, 4)
-
-                                data_joints.append(joints)
-                                data_positions.append(position)
-
-        data_joints = np.asarray(data_joints)
-        data_positions = np.asarray(data_positions)
-        # np.savez(self.filename, joints=data_joints, positions=data_positions)
-        np.savez(filename, joints=data_joints, positions=data_positions)
-
-        end = d.datetime.now()
-        print('done. duration: ', end-start)
-        return filename
-
-
-class IKTable:
-    def __init__(self, raw_data):
-        self.table = []
-
-        self.raw_data = RAW_DATA_FOLDER+self.__name_alignment(raw_data)+'.npz'
-        self.joints = []
-        self.positions = []
-        self.all_posi = []
-        # self.pos_table = [] #dict: position to joint index    #no need
-
-        self.shift_x, self.shift_y, self.shift_z = 0.855, 0.855, 0.36
-
-        self.load_data()
-        self.kd_tree()
-
-    def __name_alignment(self, name):
-        name = str(name).split('/')
-        name = name[-1].split('.')
-        return name[0]
-
-    def __density(self, data, data_dim):
-        def recur(list, dim):
-            result = []
-            dim -= 1
-            for element in list:
-                # print(len(element))
-                if not dim == 0:
-                    result += recur(element, dim)
-                else:
-                    if not len(element) == 0:
-                        result += [len(element)]
-
-            return result
-
-        return np.mean(np.array(recur(data, data_dim)))
-
-    def __str2trans(self, key_str):
-        return [float(k) for k in str(key_str)[1:-1].split(',')]
-
-    def load_data(self):
-        # s = d.datetime.now()
-        print('loading data...')
-        raw_info = np.load(self.raw_data)
-        self.joints = raw_info['joints'].tolist()
-        self.positions = [p[6].tolist() for p in raw_info['positions']]
-        self.all_posi = raw_info['positions'].tolist()
-        # print(self.positions[0])
-        # print(len(np.unique(self.positions, axis=0)))
-
-        # pos_jo = defaultdict(list)
-        # for index, pos in enumerate(raw_info['positions']):
-        #     pos_jo[str(list(pos[6]))].append(self.joints[index])
-        #
-        # self.pos_table = pos_jo
-        # self.positions = [self.__str2trans(k) for k in pos_jo.keys()]
-
-        # print(type(self.joints))
-        # print(type(self.positions))
-
-        print('loading done.')
-        # print(d.datetime.now()-s)
-
-    def switch_raw_data(self, raw_data=None):
-        if raw_data == 'empty':
-            print('new raw_data needed.')
-            return 0
-
-        self.raw_data = RAW_DATA_FOLDER+self.__name_alignment(raw_data)+'.npz'
-        self.load_data()
-        self.kd_tree()
-        print('switch to '+raw_data)
-
-    def searching_area(self, target):
-        # return a list of positions
-        for i,v in enumerate(target):
-            target[i] = round(v, 4)
-
-        target_space = self.query_kd_tree(target)
-
-        return target_space
-
-    def kd_tree(self):
-        self.table = KDTree(self.positions, leafsize=2, balanced_tree=True)
-
-    def query_kd_tree(self, target, range = 0.05):
-        # searching_space = self.table.query_ball_point(target, 0.02)
-        #
-        # target_space = []
-        # for key in searching_space:
-        #     target_space.append(self.positions[key])
-
-        # return target_space
-
-        result = self.table.query_ball_point(target, range)
-        if (len(result) < 2):
-            result = self.query_kd_tree(target, range+0.01)
-
-        return result
-
+@jit
+def diff_cal(list_1, list_2):
+    return m.sqrt(sum([(i - j)**2 for i, j in zip(list_1, list_2)]))
 
 class IKSimulator:
     def __init__(self, algo='pure'):
@@ -281,19 +104,6 @@ class IKSimulator:
 
         return nearby_postures
 
-    def posture_comparison(self, position):
-
-        nearby_postures = []
-        for i_joint, value in enumerate(target_space):
-            for i_type in nearby_postures:
-                pass
-
-
-
-                #test
-            else:
-                nearby_postures.append(value)
-
 
     def get_posts(self, indices):
         ref_joint = [3,5,6]
@@ -341,13 +151,32 @@ class IKSimulator:
         # return posture
         return message
 
-    def approximation(self, imitating_joint, target_pos, moving_joint=[i for i in range(6)]):
+    @staticmethod
+    @jit(nopython=True)
+    def approximation(imitating_joint, target_pos, moving_joint=[i for i in range(6)]):
         # 0.007915
-        start = d.datetime.now()
-
         # rad_offset = [(m.pi/180.0)*(0.5**i) for i in range(3)]  #[1, 0.5, 0.25] degree
         rad_offset = [(m.pi/180.0)*(0.5**i) for i in range(7)]  #[1, 0.5, 0.25] degree
-        diff = self.diff_cal(self.fk(imitating_joint), target_pos)
+
+        dh = np.array([[0.0,     0.0, 0.333,     0.0],\
+                       [0.0,     0.0,   0.0, -m.pi/2],\
+                       [0.0,     0.0, 0.316,  m.pi/2],\
+                       [0.0,  0.0825,   0.0,  m.pi/2],\
+                       [0.0, -0.0825, 0.384, -m.pi/2],\
+                       [0.0,     0.0,   0.0,  m.pi/2],\
+                       [0.0,   0.088, 0.107,  m.pi/2]])
+        dh[:,0] = imitating_joint
+
+        fk_mat = np.eye(4)
+        for i in range(7):
+            dh_mat = np.array([[m.cos(dh[i,0])                    , -m.sin(dh[i,0])                    ,  0                  ,  dh[i,1]                    ],\
+                      [m.sin(dh[i,0])*m.cos(dh[i,3]),  m.cos(dh[i,0])*m.cos(dh[i,3]), -m.sin(dh[i,3]), -dh[i,2]*m.sin(dh[i,3])],\
+                      [m.sin(dh[i,0])*m.sin(dh[i,3]),  m.cos(dh[i,0])*m.sin(dh[i,3]),  m.cos(dh[i,3]),  dh[i,2]*m.cos(dh[i,3])],\
+                      [0                                      ,  0                                      ,  0                  ,  1                               ]])
+            fk_mat = np.dot(fk_mat, dh_mat)
+            # print(fk_mat[:3,3])
+
+        diff = m.sqrt(sum([(i - j)**2 for i, j in zip(fk_mat[:3,3], target_pos)]))
         # print(diff)
 
         tmp_joint = imitating_joint
@@ -358,8 +187,26 @@ class IKSimulator:
                 while reverse < 2:
                     tmp_joint[i] += offset
                     pre_diff = diff
-                    tmp_pos = self.fk(tmp_joint)
-                    diff = self.diff_cal(tmp_pos, target_pos)
+                    dh = np.array([[0.0,     0.0, 0.333,     0.0],\
+                                   [0.0,     0.0,   0.0, -m.pi/2],\
+                                   [0.0,     0.0, 0.316,  m.pi/2],\
+                                   [0.0,  0.0825,   0.0,  m.pi/2],\
+                                   [0.0, -0.0825, 0.384, -m.pi/2],\
+                                   [0.0,     0.0,   0.0,  m.pi/2],\
+                                   [0.0,   0.088, 0.107,  m.pi/2]])
+                    dh[:,0] = tmp_joint
+
+                    fk_mat = np.eye(4)
+                    for i in range(7):
+                        dh_mat = np.array([[m.cos(dh[i,0])                    , -m.sin(dh[i,0])                    ,  0                  ,  dh[i,1]                    ],\
+                                  [m.sin(dh[i,0])*m.cos(dh[i,3]),  m.cos(dh[i,0])*m.cos(dh[i,3]), -m.sin(dh[i,3]), -dh[i,2]*m.sin(dh[i,3])],\
+                                  [m.sin(dh[i,0])*m.sin(dh[i,3]),  m.cos(dh[i,0])*m.sin(dh[i,3]),  m.cos(dh[i,3]),  dh[i,2]*m.cos(dh[i,3])],\
+                                  [0                                      ,  0                                      ,  0                  ,  1                               ]])
+                        fk_mat = np.dot(fk_mat, dh_mat)
+                        # print(fk_mat[:3,3])
+
+                    tmp_pos = fk_mat[:3,3]
+                    diff = m.sqrt(sum([(i - j)**2 for i, j in zip(tmp_pos, target_pos)]))
                     # print(tmp_pos, diff)
                     if diff >= pre_diff:
                         offset *= -1
@@ -368,7 +215,7 @@ class IKSimulator:
                 tmp_joint[i] += offset
                 # print('joint %s with %s done' %(i+1, offset))
 
-        end = d.datetime.now()
+
 
         # return tmp_joint, pre_diff, end-start
         return tmp_joint, pre_diff
@@ -443,6 +290,26 @@ class IKSimulator:
 
         return tmp_joint, diff
 
+    @staticmethod
+    @jit(nopython=True)
+    def best3joints(moves, tmp_joint, target_pos):
+
+        joint2move = 0
+        max = 0
+        for jo in range(6):
+            j1 = c.deepcopy(tmp_joint)
+            j2 = c.deepcopy(tmp_joint)
+            j1[jo] += moves[jo]
+            j2[jo] -= moves[jo]
+
+            vec = [round((a-b), 6) for a,b in zip(fk_dh(j1), fk_dh(j2))]
+            dotp = abs(np.dot(np.subtract(target_pos, fk_dh(tmp_joint)), vec))
+            # print(dotp)
+            if dotp > max:
+                max = dotp
+                joint2move = jo
+        return joint2move
+
     def vector_portion_v1(self, p_type, target_pos):
         # moves = [0.00753605, 0.01589324, 0.0483029, 0.03467266, 0.71936916, 0.16382559, 0.0]
         moves = [(1 * m.pi/180)]*7  #0.017453292519943295
@@ -454,20 +321,8 @@ class IKSimulator:
         loop = 0
         while diff > threshold and loop < 20:
             loop += 1
-            joint2move = 0
-            max = 0
-            for jo in range(6):
-                j1 = c.deepcopy(tmp_joint)
-                j2 = c.deepcopy(tmp_joint)
-                j1[jo] += moves[jo]
-                j2[jo] -= moves[jo]
+            joint2move = self.best3joints(moves, tmp_joint, target_pos)
 
-                vec = [round((a-b), 6) for a,b in zip(self.fk(j1),self.fk(j2))]
-                dotp = abs(np.dot(np.subtract(target_pos, self.fk(tmp_joint)), vec))
-                # print(dotp)
-                if dotp > max:
-                    max = dotp
-                    joint2move = jo
             # print(joint2move)
             tmp_joint, diff = self.approximation(tmp_joint, target_pos, moving_joint=[joint2move])
 
@@ -590,20 +445,15 @@ if __name__ == '__main__':
     # gather.without_colliding_detect('raw_data_7j_1')
 
     # table2 = IKTable('table3', 'raw_data_7j_1')
-    ik_simulator = IKSimulator()
+    # ik_simulator = IKSimulator()
     target = [0.554499999999596, -2.7401472130806895e-17, 0.6245000000018803]
     # ik_simulator.find_all_posture(target)
 
-    # s = d.datetime.now()
-    # runner(IKSimulator(algo='pure'), 500, 'result_pure')
-    # e = d.datetime.now()
-    # print('full process duration: ', e-s)
+    s = d.datetime.now()
+    runner(IKSimulator(algo='pure'), 500, 'result_pure')
+    e = d.datetime.now()
+    print('full process duration: ', e-s)
 
-    # s = d.datetime.now()
-    # runner(IKSimulator(algo='pure'), 500, 'result_pure')
-    # e = d.datetime.now()
-    # print('full process duration: ', e-s)
-    #
     # s = d.datetime.now()
     # runner(IKSimulator(algo='vp_v1'), 500, 'result_vp_v1')
     # e = d.datetime.now()
@@ -614,6 +464,6 @@ if __name__ == '__main__':
     # e = d.datetime.now()
     # print('full process duration: ', e-s)
 
-    show_avg(ik_simulator, 'result_pure_v1')
+    # show_avg(ik_simulator, 'result_pure_v1')
 
     print('end')
