546748807fd74cbfa4a316f4c5648996d92b3db5
bobbingbobb <lqqooynneb@gmail.com>
2021-12-14 15:23:34 +0800

    numba test, show_avg

--- scripts/ik_simulator.py
+++ scripts/ik_simulator.py
@@ -6,6 +6,8 @@ from collections import namedtuple, defaultdict
 import copy as c
 import random as r
 
+from numba import jit
+
 from scipy.spatial import KDTree
 
 DATA_FOLDER = '../data/'
@@ -341,6 +343,7 @@ class IKSimulator:
         # return posture
         return message
 
+    # @jit
     def approximation(self, imitating_joint, target_pos, moving_joint=[i for i in range(6)]):
         # 0.007915
         start = d.datetime.now()
@@ -414,7 +417,7 @@ class IKSimulator:
 
         # print([p.diff for p in posture])
         message = {}
-        # message['target:'] = target_pos
+        message['target:'] = target_pos
         message['posture: '] = len(posture)
         message['origin diff: '] = np.mean(origin_diff)
         message['mean diff: '] = np.mean(np.array([p.diff for p in posture]))
@@ -540,31 +543,37 @@ def runner(ik_simulator, iter, filename):
     t50 = [[0.5758, -0.4885, 1.0271], [-0.6085, -0.4255, 0.5083], [0.2788, -0.095, 0.4679], [0.2401, 0.0485, 0.4424], [-0.5991, 0.0507, 0.076], [-0.7214, 0.8085, 1.0062], [0.6651, 0.2353, 0.1987], [0.5085, -0.7104, -0.0782], [0.1461, -0.5669, 0.7866], [0.3165, 0.7707, 0.2065], [0.2235, -0.2836, 0.5669], [-0.5145, 0.1317, -0.2905], [0.7966, -0.4236, 0.8411], [-0.3434, -0.1719, 0.942], [0.1089, 0.2281, 0.3716], [-0.7805, 0.017, 0.1633], [-0.2938, 0.6114, -0.1254], [0.6441, -0.465, 0.3824], [0.5869, 0.5077, 0.6117], [-0.7016, 0.7046, 0.0695], [0.6497, -0.6365, 0.1269], [-0.0412, -0.462, 0.3256], [0.3443, 0.2157, -0.2519], [-0.476, 0.2943, 0.1508], [-0.3097, -0.6039, 0.9085], [-0.5675, -0.1751, 1.1225], [-0.3002, -0.5436, 0.9165], [-0.0303, -0.1176, 0.5681], [0.0569, 0.7381, 0.4034], [0.0173, -0.4492, 0.2811], [0.5773, -0.6733, 0.3621], [0.1824, -0.2039, 0.6849], [0.2546, -0.6338, 0.0493], [0.1626, -0.5477, 0.326], [-0.7549, 0.1028, 0.6731], [0.0792, -0.631, 0.2127], [-0.5684, -0.7224, -0.303], [0.4818, -0.2907, -0.0176], [0.124, -0.2767, -0.3508], [0.7752, 0.018, 0.315], [-0.5902, 0.3012, 1.1607], [-0.8282, -0.4573, 0.9724], [-0.4213, -0.2136, 0.0478], [-0.4584, 0.7187, -0.2936], [0.7528, -0.0771, 0.5969], [-0.0336, -0.7186, 0.5271], [-0.7806, 0.6753, 0.0032], [-0.5677, -0.2493, 0.0197], [-0.5725, 0.3716, -0.1632], [-0.4644, -0.0062, 0.7572]]
 
     message = []
-    for i in range(iter):
-        x = round(r.uniform(-0.855, 0.855), 4)
-        y = round(r.uniform(-0.855, 0.855), 4)
-        z = round(r.uniform(-0.36, 1.19), 4)
-        target = [x, y, z]
-        print(str(i+1)+': '+str(target))
-        message.append(ik_simulator.find_all_posture(target))
-        np.save('../data/result/'+filename, message)
-
-    # for i, target in enumerate(t20):
+    # for i in range(iter):
+    #     x = round(r.uniform(-0.855, 0.855), 4)
+    #     y = round(r.uniform(-0.855, 0.855), 4)
+    #     z = round(r.uniform(-0.36, 1.19), 4)
+    #     target = [x, y, z]
     #     print(str(i+1)+': '+str(target))
     #     message.append(ik_simulator.find_all_posture(target))
+    #     np.save('../data/result/'+filename, message)
 
-    np.save(''+filename, message)
+    for i, target in enumerate(t20):
+        print(str(i+1)+': '+str(target))
+        message.append(ik_simulator.find_all_posture(target))
+
+    np.save('../data/result/'+filename, message)
     print('done.')
 
 
 
-def show_avg(filename):
-    message = np.load(filename)
+def show_avg(ik_simulator, filename):
+    message = np.load('../data/result/'+filename+'.npy', allow_pickle=True)
 
     mes = defaultdict(list)
+    n = 0
     for m in message:
         for k, v in m.items():
             mes[k].append(v)
+
+        if m['mean diff: '] <= 0.005:
+            n += 1
+    print(n)
+    print(len(message))
 
     result = {}
     # print(mes)
@@ -575,7 +584,7 @@ def show_avg(filename):
             result[k] = np.average(v, axis=0, weights=mes['posture: '])
         # print(v)
 
-    ik_simulator.messenger(result)
+    # ik_simulator.messenger(result)
 
 if __name__ == '__main__':
     print('start')
@@ -583,23 +592,30 @@ if __name__ == '__main__':
     # gather.without_colliding_detect('raw_data_7j_1')
 
     # table2 = IKTable('table3', 'raw_data_7j_1')
-    # ik_simulator = IKSimulator()
+    ik_simulator = IKSimulator()
     target = [0.554499999999596, -2.7401472130806895e-17, 0.6245000000018803]
     # ik_simulator.find_all_posture(target)
 
-    s = d.datetime.now()
-    runner(IKSimulator(algo='pure'), 500, 'result_pure')
-    e = d.datetime.now()
-    print('full process duration: ', e-s)
+    # s = d.datetime.now()
+    # runner(IKSimulator(algo='pure'), 500, 'result_pure')
+    # e = d.datetime.now()
+    # print('full process duration: ', e-s)
 
-    s = d.datetime.now()
-    runner(IKSimulator(algo='vp_v1'), 500, 'result_vp_v1')
-    e = d.datetime.now()
-    print('full process duration: ', e-s)
+    # s = d.datetime.now()
+    # runner(IKSimulator(algo='pure'), 500, 'result_pure')
+    # e = d.datetime.now()
+    # print('full process duration: ', e-s)
+    #
+    # s = d.datetime.now()
+    # runner(IKSimulator(algo='vp_v1'), 500, 'result_vp_v1')
+    # e = d.datetime.now()
+    # print('full process duration: ', e-s)
+    #
+    # s = d.datetime.now()
+    # runner(IKSimulator(algo='vp_v2'), 500, 'result_vp_v2')
+    # e = d.datetime.now()
+    # print('full process duration: ', e-s)
 
-    s = d.datetime.now()
-    runner(IKSimulator(algo='vp_v2'), 500, 'result_vp_v2')
-    e = d.datetime.now()
-    print('full process duration: ', e-s)
+    show_avg(ik_simulator, 'result_pure_v1')
 
     print('end')
